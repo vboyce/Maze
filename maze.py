eeb0a360a3d2904cf8599f1803aedaecdadec740
Veronica Boyce <vboyce@mit.edu>
2018-02-06 16:45:12 -0500

    Added word list and word list checking, maze.py is now command-line runnable, takes a file in instead Note: doc strings need updating

--- maze.py
+++ maze.py
@@ -1,6 +1,45 @@
 import numpy
+import csv
+import sys
+###Settings:###
+# Set these to specify what options you want#
+#options for mode are "nonce" and "anagram"
+mode="anagram"
+#options for dashes are False and True
+dashes=True
+#specify a file name for the output
+output_file="ibex-master/data_includes/ibex_data.js"
+#what ibex stuff goes at the top of the file
+header=('var shuffleSequence = seq("intro",followEachWith("sep","test"), "done");\n\n'
+    'var showProgressBar =false;\n\n'
+    'var items = [\n\n'
+    '\t["intro", "Message", {html: "This is a demo of the maze task implemented in ibex.'
+    ' You will read a sentence word by word. On each screen you will see two words, one of'
+    ' which is a reasonable continuation of the sentence. Use your left and right arrow '
+    'buttons to indicate which word is the correct continuation. The sentence will start with \'the\'." }],'
+    '\n\t["sep", "MazeSeparator", {normalMessage: "Correct! Press any key to continue",'
+    ' errorMessage: "Incorrect! Press any key to continue."}],'
+    '\n\t["done", "Message", {html: "All done!"}]')
+#what ibex stuff goes towards the end of the file
+footer=('\n];')
+#define global variables WORDLIST and ALPHABET
+with open("celex_words.txt") as celex:
+    words=celex.readlines()
+words=[x.strip().lower() for x in words]
+WORDLIST=set(words)
+ALPHABET="abcdefghijklmnoqrstuvwxyz"
 
-x=["The dog went to the park.", "Look, a rainbow!"]
+
+def read_input(file):
+    '''given a file name/path, reads in contents of file into two line by line list.'''
+    sentences=[]
+    items=[]
+    with open(file) as f:
+        line=csv.reader(f, delimiter='\t')
+        for row in line:
+            sentences.append((row[0]))
+            items.append((row[1]))
+    return (sentences, items)
 
 def distractor(data, mode="nonce", dashed=False):
     '''Given an array of sentences, returns a same size array of distractor word sentences.
@@ -18,11 +57,11 @@ def distractor(data, mode="nonce", dashed=False):
                 new=nonce(lower)
             if mode=="anagram":
                 new=anagram(lower)
-            new_words.append(redo_case("".join(new), case, punc))
+            new_words.append(redo_case(new, case, punc))
         if dashed:
             nonsense="--- "+" ".join(new_words[1:])
         else:
-            nonsense=" ".join(nonce)
+            nonsense=" ".join(new_words)
         output.append(nonsense)
     return(output)
 
@@ -45,36 +84,40 @@ def redo_case(word, case, punc):
 
 def nonce(word):
     '''returns string of random letters of length word'''
-    alphabet="abcdefghijklmnoqrstuvwxyz"
-    return (numpy.random.choice(list(alphabet), (len(word))))
+    for i in range(10):
+        test="".join(numpy.random.choice(list(ALPHABET), (len(word))))
+        if test not in WORDLIST:
+            return test
+    return(nonce(word+"a"))
 
 def anagram(word):
-    '''takes a string and returns an anagram, guaranteed to not be the same as the starting word
-    exception: for one letter words, returns some random one letter word'''
-    if len(word)==1:
-        return nonce(word)
-    else:
-        nagaram=list(word)
-        while (numpy.array_equiv(nagaram, list(word))):
-            nagaram=numpy.random.choice(list(word), len(word), replace=False)
-        return nagaram
+    '''takes a string and returns an anagram '''
+    for i in range(10):
+        nagaram="".join(numpy.random.choice(list(word), len(word), replace=False))
+        if nagaram not in WORDLIST:
+            return nagaram
+    print("Warning, can't find valid angram of "+word)
+    new_word="".join(numpy.random.choice(list(ALPHABET), 1))+word
+    return(anagram(new_word))
 
-def ibex_format(item_name, sentences, distractors, file=None):
+def ibex_format(item_name, sentences, distractors,header, footer, file=None, ):
     '''given a list of item names, a list of sentences and a list of distrator items, produces a string suitable
 for including in the items list of an ibex experiment. If a file is given, writes to there as well.'''
     if not len(item_name)==len(sentences):
         raise Exception("item_name and sentences are not same length")
     if not len(sentences)==len(distractors):
         raise Exception("sentences and distractors are not the same length")
-    items=""
+    items=header
     for i in range(len(sentences)):
-        items+=("[\""+item_name[i]+"\", \"Maze\", {s: \""+sentences[i]+"\", a: \""+distractors[i]+"\"}],\n")
+        items+=(",\n\t[\""+item_name[i]+"\", \"Maze\", {s: \""+sentences[i]+"\", a: \""+distractors[i]+"\"}]")
+    items+=footer
     if file:
         f=open(file, "w")
         f.writelines(items)
     return items
-                   
-#ibex_format(["test", "test"], x, distractor(x, dashed=True))
 
-
-
+if __name__ == "__main__":
+    filename=sys.argv[1]
+    sentences, items=read_input(filename)
+    distractors=distractor(sentences, mode, dashes)
+    ibex_format(items, sentences, distractors, header, footer, output_file)
