7043957e4d724f64d55a8808c933008c2d1d8d1e
Veronica Boyce <vboyce@mit.edu>
2018-11-05 22:19:32 -0500

    Fix alternate selection

--- maze_automate/automate.py
+++ maze_automate/automate.py
@@ -21,18 +21,16 @@ def get_unigram_freq(word):
 
 def get_alternates(word):
     '''given a word, returns candidate buddies'''
-    alts=LEXICON.get((len(word),get_unigram_freq(word)))
-    if len(alts)>50: #have enough options
-        return alts
-    else: #not enough options, opt for slightly less frequent
-        alts.extend(LEXICON.get((len(word), get_unigram_freq(word)-1))
-        if len(alts)>50:
-            print ("Not enough neighbors for "+word)
-            return alts
-        else:
-            print("Really not enough neighbors for "+word)
-            return alts.append(word)
-        
+    alts=[]
+    i=0
+    while len(alts)<50:
+        a=LEXICON.get((len(word)+i, get_unigram_freq(word)))
+        if a!=None:
+            alts.extend(a)
+        i+=1
+    if i>1:
+        print("Trouble finding neighbors for "+word)
+    return alts
 
 def split_sentence(sentence):
     '''takes sentence, returns list of word-units. one day might do parsing with punctuation'''
@@ -53,7 +51,9 @@ def pre_surprisal(sentences):
                 punct=""
             alts=get_alternates(word)
             alts_to_use=[]
-            while len(alts_to_use)<20:
+            for i in range(50):
+                if len(alts_to_use)>19:
+                    break
                 alt=random.choice(alts)
                 if alt not in alts_to_use:
                     if alt==alt.lower():
@@ -66,35 +66,32 @@ def output(sentences, g_s, one_b_s, filename):
     f=open(filename, "a")
     for i in range(len(g_s)):
         f.write("\n\n"+sentences[i]+"\n")
-        f.write("\n"+g_s[i][0]+"\n")
-        f.write("\n"+one_b_s[i][0]+"\n\n")
         words=split_sentence(sentences[i])
         for j in range(len(g_s[i][1])):
             f.write(" ".join(words[:(j+1)])+"\n\n") # context 
-            f.write(" | "+words[j+1]+" | g: "+str(round(g_s[i][1][j]))+ " | 1_b: "+str(round(one_b_s[i][1][j])) + " |\n")
+            f.write(" | "+words[j+1]+" | g: "+str(int(round(g_s[i][1][j])))+ " | 1_b: "+str(int(round(one_b_s[i][1][j]))) + " |\n")
             f.write("|---|---|---|\n")
             for k in g_s[i][2][j]:
-                f.write(" | "+k+ " | "+str(round(g_s[i][2][j][k]))+" | "+str(round(one_b_s[i][2][j][k]))+" |\n")
+                f.write(" | "+k+ " | "+str(int(round(g_s[i][2][j][k])))+" | "+str(int(round(one_b_s[i][2][j][k])))+" |\n")
             f.write("\n")
     f.close()
 
 def read_sentences(filename):
     f=open(filename, "r")
-    sentences =[x.strip for x in f.readlines()]
+    sentences =[x.strip() for x in f.readlines()]
     f.close()
     return sentences
  
-def process_sentences(in_file, out_filename):
+def process_sentences(in_file, out_file):
     '''given sentence, file location, writes all sorts of useful stuff to file'''
     sentences=read_sentences(in_file)
     to_test=pre_surprisal(sentences)
     g_surprisal = g.Surprisal(to_test)
     one_b_surprisal = one_b.Surprisal(to_test)
-    output(sentences, g_surprisal, one_b_surprisal, filename)
+    output(sentences, g_surprisal, one_b_surprisal, out_file)
 
 def check_lexicon():
     for key in sorted(LEXICON):
         print(key,"\t",len(LEXICON[key]))
 
-### end deprecated
 process_sentences("input.txt", "output_4.md")
