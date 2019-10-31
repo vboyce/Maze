fd90a6680b0eeff6f6f54a56de7af1b111257b67
rayaburong <arun92166@gmail.com>
2019-10-31 01:07:01 -0400

    groundwork for flexible matching

--- maze_automate/automate_new.py
+++ maze_automate/automate_new.py
@@ -23,6 +23,8 @@ parser.add_argument('--min_rel', type=int, default=-1,
                     help='relative threshold of surprisal for a bad word to compare with the good word')
 parser.add_argument('--duplicate_words', action="store_true", default=False,
                     help='allow duplicate words to be in the same output sentence')
+parser.add_argument('--matching', choices=["index", "manual", "auto"], default="index",
+                    help='between santences with the same item number, which word matching method to be used (index, manual or auto)')
 args = parser.parse_args()
 
 def save_output(outfile, item_to_info, end_result):
@@ -44,7 +46,8 @@ def save_output(outfile, item_to_info, end_result):
                 f.write('"'+item_to_info[key][0][i]+'";')
                 f.write('"'+key+'";')
                 f.write('"'+item_to_info[key][1][i]+'";')
-                f.write('"'+end_result[item_to_info[key][1][i]]+'"\n')
+                f.write('"'+end_result[item_to_info[key][1][i]]+'";')
+                f.write('"'+item_to_info[key][2][i]+'"\n')
 
 def save_ibex_format(outfile, item_to_info, end_result):
     '''Saves results to a file in ibex format
@@ -60,9 +63,10 @@ def save_ibex_format(outfile, item_to_info, end_result):
                 f.write('[["'+item_to_info[key][0][i]+'", ')
                 f.write(key+'], "Maze", {s:')
                 f.write('"'+item_to_info[key][1][i]+'", a:')
-                f.write('"'+end_result[item_to_info[key][1][i]]+'"}], \n')
+                f.write('"'+end_result[item_to_info[key][1][i]]+'";')
+                f.write('"'+item_to_info[key][2][i]+'"\n')
 
-def read_input(filename):
+def read_input(filename, match_type):
     '''Reads an input file
     Arguments:
     filename = a semicolon delimited file with the following information
@@ -71,6 +75,7 @@ def read_input(filename):
     second column = item number. Sentences that share item number will get same distractors,
     and *Must* have same number of word.
     Third column = sentence
+    Fourth column = word keys (ids given to each word to indicate that they would match to the same bad_word
     Returns:
     item_to_info = a dictionary of item numbers as keys and a pair of lists (conditions, sentences) as value
     sentences =  a list of sentences grouped by item number (ie will get matching distractors)'''
@@ -81,14 +86,22 @@ def read_input(filename):
             if row[1] in item_to_info: #item num already seen
                 item_to_info[row[1]][0].append(row[0]) #add condition to the list
                 item_to_info[row[1]][1].append(row[2].strip()) #add sentence to the list
+                if match_type == 'index' or match_type == 'auto': #auto has not been implemented
+                    item_to_info[row[1]][2].append([i for i in range(len(row[2].strip()])
+                else:
+                    item_to_info[row[1]][2].append(row[3].strip()) #add word keys to the list
             else:
                 item_to_info[row[1]] = [[row[0]], [row[2].strip()]] # new item num, add a new entry
+                if match_type == 'index' or match_type == 'auto': #auto has not been implemented
+                    item_to_info[row[1]].append([[i for i in range(len(row[2].strip()]])
+                else:
+                    item_to_info[row[1]][2].append([row[3].strip()]) #add word keys to the list
     sentences = []
     for item in sorted(item_to_info):
-        sentences.append(item_to_info[item][1]) #make a list of sentences, grouped by item number
+        sentences.append((item_to_info[item][1], item_to_info[item][2])) #make a list of sentences and word keys, grouped by item number
     return (item_to_info, sentences)
 
-def run(which_model, freq, sentences, num_to_test, minimum, backup_min, duplicate_words):
+def run(which_model, freq, sentences_keys, num_to_test, min_abs, min_rel, duplicate_words):
     '''wrapper for using either model with either wordfreq to use
     Arguments:
     model = the model specified
@@ -107,12 +120,13 @@ def run(which_model, freq, sentences, num_to_test, minimum, backup_min, duplicat
         dictionary = one_b_model.load_dict()
 
     for i, _ in enumerate(sentences): #process all the sentences
+        (sentences, keys) = sentences_keys[i] #divide the (sentences, keys) tuple into sentences and word keys
         if which_model == "gulordava":
-            bad = gulordava_model.do_sentence_set(sentences[i], model, device, dictionary, ntokens, num_to_test, minimum, backup_min, duplicate_words)
+            bad = gulordava_model.do_sentence_set(sentences, model, device, dictionary, ntokens, num_to_test, min_abs, min_rel, duplicate_words)
         elif which_model == "one_b":
-            bad = one_b_model.do_sentence_set(sentences[i], sess, t, dictionary)
-        for j, _ in enumerate(sentences[i]): #record results
-            end_result[sentences[i][j]] = bad[j]
+            bad = one_b_model.do_sentence_set(sentences, sess, t, dictionary)
+        for j, _ in enumerate(sentences): #record results
+            end_result[sentences[j]] = bad[j]
     return end_result
 
 '''Takes input, generates distractors, writes to output file
@@ -122,10 +136,9 @@ outfile = where to write output to
 lang_model = either "gulordava" or "one_b" for which language model to use
 out_format = either "basic" (for a semicolon delimited output) or "ibex" for ibex ready output
 Returns: none'''
-item_to_info, sentences = read_input(args.input) # read input
-num_to_test = args.num_to_test #set num_to_test in find_bad_words
-print("Number of bad words to test = " + str(num_to_test))
-end_result = run(args.model, args.freq, sentences, num_to_test, args.min_abs, args.min_rel, args.duplicate_words)
+item_to_info, sentences = read_input(args.input, args.matching) # read input
+print("Number of bad words to test = " + str(args.num_to_test))
+end_result = run(args.model, args.freq, sentences, args.num_to_test, args.min_abs, args.min_rel, args.duplicate_words)
 if args.format == "ibex": #save output
     save_ibex_format(args.output, item_to_info, end_result)
 elif args.format =="basic": # save output
